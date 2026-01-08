7afd336f9899a8b1c966b683da7350e61b83e801
Satyam-Private <satyampatil1252@gmail.com>
2026-01-09 02:39:29 +0530

    updated cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,7 +22,7 @@ import stockAgingRoutes from './modules/stock/stockAging.routes.js'
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://backend-8taj.onrender.com',
     credentials: true
 }));
 
